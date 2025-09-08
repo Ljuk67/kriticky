7e73f840ffa952eb2bb0810d751552469b963c20
Lukas Cech <24230613+ljuk67@users.noreply.github.com>
2025-09-09 00:20:39 +0200

    Stop tracking Astro cache (.astro)

--- .astro/content-assets.mjs
+++
@@ -1,6 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_MGbyL from "../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
-import __ASTRO_IMAGE_IMPORT_Z1Yq4sn from "../../assets/blog-placeholder-about.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md";
-import __ASTRO_IMAGE_IMPORT_Z1F4X4R from "../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md";
-export default new Map([["../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_MGbyL], ["../../assets/blog-placeholder-about.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md", __ASTRO_IMAGE_IMPORT_Z1Yq4sn], ["../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md", __ASTRO_IMAGE_IMPORT_Z1F4X4R]]);
-		
