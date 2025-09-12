3d766a711822c1772ce2643d78a2f6c9e60556b0
Lukas Cech <24230613+ljuk67@users.noreply.github.com>
2025-09-12 20:42:24 +0200

    mobile layout global css

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Kritické myslenie v praxi';
-export const SITE_DESCRIPTION = 'Ako oddeliť zrná od pliev v záplave informácií.';
+export const SITE_DESCRIPTION = 'Ako oddeliť zrná od pliev v informačnom chaose.';
