7388e23555f14cf18210c4040b0bb40308d0d751
Lukas Cech <24230613+ljuk67@users.noreply.github.com>
2025-09-12 20:34:09 +0200

    layout improvements

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Kritické myslenie';
-export const SITE_DESCRIPTION = 'Vitajte na webe o kritickom myslení.';
+export const SITE_TITLE = 'Kritické myslenie v praxi';
+export const SITE_DESCRIPTION = 'Ako oddeliť zrná od pliev v záplave informácií.';
